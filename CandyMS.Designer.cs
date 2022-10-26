7ad51597071c07ea516be56cca3d8edfd173341b
AHussain101 <107582721+AHussain101@users.noreply.github.com>
2022-10-25 23:48:19 -0700

    Create CandyMS.Designer.cs

---
+++ CandyMS.Designer.cs
@@ -0,0 +1,233 @@
+namespace AarfansCandyStore_AarfanH3
+{
+    partial class CandyMS
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.LollipopScreen = new System.Windows.Forms.Button();
+            this.ChocolateScreen = new System.Windows.Forms.Button();
+            this.HardcandyScreen = new System.Windows.Forms.Button();
+            this.SelectionTitle = new System.Windows.Forms.Label();
+            this.LolliChoiceSelectionDisplay = new System.Windows.Forms.Label();
+            this.ChocChoiceSelectionDisplay = new System.Windows.Forms.Label();
+            this.HardChoiceSelectionDisplay = new System.Windows.Forms.Label();
+            this.LollipopTextDisplay = new System.Windows.Forms.TextBox();
+            this.ChocolateTextDisplay = new System.Windows.Forms.TextBox();
+            this.HardCandyTextDisplay = new System.Windows.Forms.TextBox();
+            this.PriceofItems = new System.Windows.Forms.Label();
+            this.LollipopPriceText = new System.Windows.Forms.TextBox();
+            this.ChocolatePriceText = new System.Windows.Forms.TextBox();
+            this.CandyPriceText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter your name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(255, 26);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(152, 23);
+            this.UserName.TabIndex = 1;
+            // 
+            // LollipopScreen
+            // 
+            this.LollipopScreen.Location = new System.Drawing.Point(104, 115);
+            this.LollipopScreen.Name = "LollipopScreen";
+            this.LollipopScreen.Size = new System.Drawing.Size(105, 23);
+            this.LollipopScreen.TabIndex = 2;
+            this.LollipopScreen.Text = "Lollipop Choices";
+            this.LollipopScreen.UseVisualStyleBackColor = true;
+            this.LollipopScreen.Click += new System.EventHandler(this.LollipopScreen_Click);
+            // 
+            // ChocolateScreen
+            // 
+            this.ChocolateScreen.Location = new System.Drawing.Point(215, 115);
+            this.ChocolateScreen.Name = "ChocolateScreen";
+            this.ChocolateScreen.Size = new System.Drawing.Size(117, 23);
+            this.ChocolateScreen.TabIndex = 3;
+            this.ChocolateScreen.Text = "Chocolate Choices";
+            this.ChocolateScreen.UseVisualStyleBackColor = true;
+            this.ChocolateScreen.Click += new System.EventHandler(this.ChocolateScreen_Click);
+            // 
+            // HardcandyScreen
+            // 
+            this.HardcandyScreen.Location = new System.Drawing.Point(338, 115);
+            this.HardcandyScreen.Name = "HardcandyScreen";
+            this.HardcandyScreen.Size = new System.Drawing.Size(127, 23);
+            this.HardcandyScreen.TabIndex = 4;
+            this.HardcandyScreen.Text = "Hard Candy Choices";
+            this.HardcandyScreen.UseVisualStyleBackColor = true;
+            this.HardcandyScreen.Click += new System.EventHandler(this.HardcandyScreen_Click);
+            // 
+            // SelectionTitle
+            // 
+            this.SelectionTitle.AutoSize = true;
+            this.SelectionTitle.Location = new System.Drawing.Point(104, 179);
+            this.SelectionTitle.Name = "SelectionTitle";
+            this.SelectionTitle.Size = new System.Drawing.Size(90, 15);
+            this.SelectionTitle.TabIndex = 5;
+            this.SelectionTitle.Text = "Your Selections:";
+            // 
+            // LolliChoiceSelectionDisplay
+            // 
+            this.LolliChoiceSelectionDisplay.AutoSize = true;
+            this.LolliChoiceSelectionDisplay.Location = new System.Drawing.Point(104, 236);
+            this.LolliChoiceSelectionDisplay.Name = "LolliChoiceSelectionDisplay";
+            this.LolliChoiceSelectionDisplay.Size = new System.Drawing.Size(93, 15);
+            this.LolliChoiceSelectionDisplay.TabIndex = 6;
+            this.LolliChoiceSelectionDisplay.Text = "Lollipop Choice:";
+            // 
+            // ChocChoiceSelectionDisplay
+            // 
+            this.ChocChoiceSelectionDisplay.AutoSize = true;
+            this.ChocChoiceSelectionDisplay.Location = new System.Drawing.Point(104, 281);
+            this.ChocChoiceSelectionDisplay.Name = "ChocChoiceSelectionDisplay";
+            this.ChocChoiceSelectionDisplay.Size = new System.Drawing.Size(104, 15);
+            this.ChocChoiceSelectionDisplay.TabIndex = 7;
+            this.ChocChoiceSelectionDisplay.Text = "Chocolate Choice:";
+            // 
+            // HardChoiceSelectionDisplay
+            // 
+            this.HardChoiceSelectionDisplay.AutoSize = true;
+            this.HardChoiceSelectionDisplay.Location = new System.Drawing.Point(104, 317);
+            this.HardChoiceSelectionDisplay.Name = "HardChoiceSelectionDisplay";
+            this.HardChoiceSelectionDisplay.Size = new System.Drawing.Size(124, 15);
+            this.HardChoiceSelectionDisplay.TabIndex = 8;
+            this.HardChoiceSelectionDisplay.Text = "Hard Candy Selection:";
+            // 
+            // LollipopTextDisplay
+            // 
+            this.LollipopTextDisplay.Location = new System.Drawing.Point(238, 227);
+            this.LollipopTextDisplay.Name = "LollipopTextDisplay";
+            this.LollipopTextDisplay.Size = new System.Drawing.Size(153, 23);
+            this.LollipopTextDisplay.TabIndex = 9;
+            // 
+            // ChocolateTextDisplay
+            // 
+            this.ChocolateTextDisplay.Location = new System.Drawing.Point(238, 278);
+            this.ChocolateTextDisplay.Name = "ChocolateTextDisplay";
+            this.ChocolateTextDisplay.Size = new System.Drawing.Size(153, 23);
+            this.ChocolateTextDisplay.TabIndex = 10;
+            // 
+            // HardCandyTextDisplay
+            // 
+            this.HardCandyTextDisplay.Location = new System.Drawing.Point(238, 317);
+            this.HardCandyTextDisplay.Name = "HardCandyTextDisplay";
+            this.HardCandyTextDisplay.Size = new System.Drawing.Size(153, 23);
+            this.HardCandyTextDisplay.TabIndex = 11;
+            // 
+            // PriceofItems
+            // 
+            this.PriceofItems.AutoSize = true;
+            this.PriceofItems.Location = new System.Drawing.Point(427, 179);
+            this.PriceofItems.Name = "PriceofItems";
+            this.PriceofItems.Size = new System.Drawing.Size(105, 15);
+            this.PriceofItems.TabIndex = 12;
+            this.PriceofItems.Text = "Price and Quantity";
+            // 
+            // LollipopPriceText
+            // 
+            this.LollipopPriceText.Location = new System.Drawing.Point(427, 227);
+            this.LollipopPriceText.Name = "LollipopPriceText";
+            this.LollipopPriceText.Size = new System.Drawing.Size(128, 23);
+            this.LollipopPriceText.TabIndex = 13;
+            // 
+            // ChocolatePriceText
+            // 
+            this.ChocolatePriceText.Location = new System.Drawing.Point(427, 278);
+            this.ChocolatePriceText.Name = "ChocolatePriceText";
+            this.ChocolatePriceText.Size = new System.Drawing.Size(128, 23);
+            this.ChocolatePriceText.TabIndex = 14;
+            // 
+            // CandyPriceText
+            // 
+            this.CandyPriceText.Location = new System.Drawing.Point(427, 317);
+            this.CandyPriceText.Name = "CandyPriceText";
+            this.CandyPriceText.Size = new System.Drawing.Size(128, 23);
+            this.CandyPriceText.TabIndex = 15;
+            // 
+            // CandyMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CandyPriceText);
+            this.Controls.Add(this.ChocolatePriceText);
+            this.Controls.Add(this.LollipopPriceText);
+            this.Controls.Add(this.PriceofItems);
+            this.Controls.Add(this.HardCandyTextDisplay);
+            this.Controls.Add(this.ChocolateTextDisplay);
+            this.Controls.Add(this.LollipopTextDisplay);
+            this.Controls.Add(this.HardChoiceSelectionDisplay);
+            this.Controls.Add(this.ChocChoiceSelectionDisplay);
+            this.Controls.Add(this.LolliChoiceSelectionDisplay);
+            this.Controls.Add(this.SelectionTitle);
+            this.Controls.Add(this.HardcandyScreen);
+            this.Controls.Add(this.ChocolateScreen);
+            this.Controls.Add(this.LollipopScreen);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "CandyMS";
+            this.Text = "Candy Store";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.Button LollipopScreen;
+        private System.Windows.Forms.Button ChocolateScreen;
+        private System.Windows.Forms.Button HardcandyScreen;
+        private System.Windows.Forms.Label SelectionTitle;
+        private System.Windows.Forms.Label LolliChoiceSelectionDisplay;
+        private System.Windows.Forms.Label ChocChoiceSelectionDisplay;
+        private System.Windows.Forms.Label HardChoiceSelectionDisplay;
+        private System.Windows.Forms.TextBox LollipopTextDisplay;
+        private System.Windows.Forms.TextBox ChocolateTextDisplay;
+        private System.Windows.Forms.TextBox HardCandyTextDisplay;
+        private System.Windows.Forms.Label PriceofItems;
+        private System.Windows.Forms.TextBox LollipopPriceText;
+        private System.Windows.Forms.TextBox ChocolatePriceText;
+        private System.Windows.Forms.TextBox CandyPriceText;
+    }
+}
+
